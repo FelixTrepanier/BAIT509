d73dffff6708a6ba69979b0218f2a9dc9d18768d
FelixTrepanier <36861377+FelixTrepanier@users.noreply.github.com>
2018-03-26 10:45:01 -0700

    Add files via upload

---
+++ my_folder/Class 9.R
@@ -0,0 +1,18 @@
+suppressPackageStartupMessages(library(tidyverse))
+knitr::opts_chunk$set(fig.width=6, fig.height=3, fig.align="center")
+
+'
+1. x1-7.5=0 would be the classification boundary. According to this, th new point x1=6 and x2=10 would be classified as A.
+
+2. The margin is of size 2
+
+3. Observations 6, 7, 8, 9, and 10 receive a penalty. The support vector is composed of observation 1 to 5 and observations 11 to 18.
+
+4. The total penalty is 7.5
+
+5. No
+
+6. Yes they are separable. (1/11.5)x2 + (1/14)x1 - 11.5 = 0
+
+' 
+
